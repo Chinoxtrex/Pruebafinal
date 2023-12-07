876ca414d0a3b6575a5d15ea13f98bd9315d85e3
Diego <dramos083g@gmail.com>
2023-12-06 19:49:58 -0600

    7ta

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Holaaefefefefe</Text>
+      <Text>hokedspofs</Text>
       <StatusBar style="auto" />
     </View>
   );
