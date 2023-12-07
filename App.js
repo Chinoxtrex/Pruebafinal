e8fc2559a99b2ec17fae3d0f909da0f4c58cf111
Diego <dramos083g@gmail.com>
2023-12-06 19:41:15 -0600

    tercera act

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola como estas </Text>
+      <Text>Hola bien</Text>
       <StatusBar style="auto" />
     </View>
   );
